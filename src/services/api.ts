e62d1831ba779f3e266d6eb7e7be3562b3ae1559
Larissa Cravo Carvalho Câmara Santos <153231533+L4r1ss4Cr4v0@users.noreply.github.com>
2024-12-31 16:29:03 -0300

    Delete src directory

--- src/services/api.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "http://192.168.0.54:3333",
-  timeout: 2000,
-});
--- src/styles/font-family.ts
+++
@@ -1,8 +0,0 @@
-export const fontFamily = {
-  bold: "Rubik_700Bold",
-  medium: "Rubik_500Medium",
-  regular: "Rubik_400Regular",
-  semiBold: "Rubik_600SemiBold",
-};
-
-//npx expo install expo-font @expo-google-fonts/rubik
