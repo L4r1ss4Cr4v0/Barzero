74be63da87eb8df2c6e5eec1d1639e0a5fd730b7
Larissa Cravo Carvalho Câmara Santos <153231533+L4r1ss4Cr4v0@users.noreply.github.com>
2024-12-31 16:34:26 -0300

    chore: Initial commit

---
+++ src/controllers/categories-controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+
+class CategoriesController {
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      const categories = await prisma.category.findMany();
+
+      return response.json(categories);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export { CategoriesController };
